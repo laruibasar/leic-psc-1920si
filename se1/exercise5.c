bdcaaeccfdc0af418d6f475c4e284965d7b88a90
Luis Bandarra <luis.bandarra@homestudio.pt>
2019-10-30 22:41:19 +0000

    Start exercise 5 resolution

---
+++ se1/exercise5.c
@@ -0,0 +1,18 @@
+/*-
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright (c) 2019 Luis Bandarra <luis.bandarra@homestudio.pt>
+ * All rights reserved.
+ */
+
+#include <stdio.h>
+
+int
+main(int argc, char *argv[])
+{
+	printf("\nStart exercise 5...\n");
+
+	printf("N. args: %d\n", argc);
+	printf("Arg: %s\n", argv[1]);
+	return 0;
+}
